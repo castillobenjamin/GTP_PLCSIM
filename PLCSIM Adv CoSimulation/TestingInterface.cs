de09c80f507fea2ea645035d7deb453eabbfc52d
castillo.benjamin <castillo.soto.david.benjamin@bne.muratec.co.jp>
2023-04-21 17:40:12 +0900

    Add Region layout and input common methods.

--- PLCSIM Adv CoSimulation/TestingInterface.cs
+++ PLCSIM Adv CoSimulation/TestingInterface.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PLCSIM_Adv_CoSimulation.Models;
+using PLCSIM_Adv_CoSimulation.Models.Configuration;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,26 +14,106 @@ namespace PLCSIM_Adv_CoSimulation
 {
     public partial class TestingInterface : Form
     {
-        public TestingInterface()
+        #region Fields
+        private readonly CoSimulation CoSimulationInstance;
+
+        #endregion // Fields
+
+        #region Initialization
+
+        public TestingInterface(CoSimulation coSimulationInstance)
         {
             InitializeComponent();
+            CoSimulationInstance = coSimulationInstance;
         }
 
-        #region Configuration
+        #endregion // Initialization
 
-        #endregion // Configuration
+        #region Configuration
 
         private void TextBox_ProgramVersion_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        #region Input
+        #endregion // Configuration
 
-        #endregion // Input
+        #region Methods
 
-        #region Output
+        #region Update input
 
-        #endregion // Output
+        #region CELL
+
+        #endregion // CELL
+
+        #region Subsystems
+
+        #region Aisles
+
+        private void PressAisleEstopButton(int aisleNum) 
+        {
+            bool isPressSuccess = TurnInputOn(CoSimulationInstance.AlphaBotSystem.Aisles[aisleNum].OperationBox.EmergencyBtn);
+        }
+
+        #endregion // Aisles
+
+        #region Decks
+
+        #endregion // Decks
+
+        #region Dynamic Work Stations
+
+        #endregion // Dynamic Work Stations
+
+        #region Stoppers
+
+        #endregion // Stoppers
+
+        #region Evacuation rails
+
+        #endregion // Evacuation rails
+
+        #region Fire shutter
+
+        #endregion // Fire shutter
+
+        #endregion // Subsystems
+
+        #region Common methods
+        // Common for all buttons. Including E-stop, Reset, Request.
+        private bool TurnInputOn(PlcInput button) 
+        {
+            try
+            {
+                button.Value = true;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private bool TurnInputOff(PlcInput button)
+        {
+            try
+            {
+                button.Value = false;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        #endregion // Common methods
+
+        #endregion // Update input
+
+        #region Check output
+
+        #endregion // Check output
+
+        #endregion // Methods
     }
 }
