5584b6cff1224adf161fd007e2a6b3a8085bd2ce
castillo.benjamin <castillo.soto.david.benjamin@bne.muratec.co.jp>
2023-04-21 12:36:41 +0900

    Create testing interface class.

---
+++ PLCSIM Adv CoSimulation/TestingInterface.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PLCSIM_Adv_CoSimulation
+{
+    partial class TestingInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.TextBox_ProgramVersion = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 273);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(352, 88);
+            this.listBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TextBox_ProgramVersion);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(304, 162);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Meta data";
+            // 
+            // TextBox_ProgramVersion
+            // 
+            this.TextBox_ProgramVersion.Location = new System.Drawing.Point(113, 12);
+            this.TextBox_ProgramVersion.Name = "TextBox_ProgramVersion";
+            this.TextBox_ProgramVersion.Size = new System.Drawing.Size(181, 19);
+            this.TextBox_ProgramVersion.TabIndex = 2;
+            this.TextBox_ProgramVersion.TextChanged += new System.EventHandler(this.TextBox_ProgramVersion_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Pogram version";
+            // 
+            // TestingInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 373);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "TestingInterface";
+            this.Text = "TestingInterface";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox TextBox_ProgramVersion;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ PLCSIM Adv CoSimulation/TestingInterface.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PLCSIM_Adv_CoSimulation
+{
+    public partial class TestingInterface : Form
+    {
+        public TestingInterface()
+        {
+            InitializeComponent();
+        }
+
+        #region Configuration
+
+        #endregion // Configuration
+
+        private void TextBox_ProgramVersion_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Input
+
+        #endregion // Input
+
+        #region Output
+
+        #endregion // Output
+    }
+}
